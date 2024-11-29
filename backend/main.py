0d4ee9c18d2f88d8495d6f52bfe5939938dda939
mohamed-moslemani <moslemanomohamed@gmail.com>
2024-11-29 23:00:55 +0200

    backend structue change

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.routers.auth_routes import router as auth_router
+from app.routers.algorithm_simulations import router as algo_router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"], 
+)
+
+app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(algo_router, prefix="/algorithms", tags=["simulations"])
+
+@app.get("/")
+async def root():
+    return {"message": "مرحبًا بكم في AI in Arabiya"}
